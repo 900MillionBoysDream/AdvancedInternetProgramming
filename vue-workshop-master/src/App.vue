f99f22db828cb62a81cacd01889595bd69dfa5d9
900MillionBoysDream <Jiayi.Shi@student.uts.edu.au>
2018-08-18 15:01:18 +1000

    Attended the vue.js workshop and accomplished the exercise

---
+++ vue-workshop-master/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="sports-field">
+      <h1 style="color: palevioletred;">{{ title }}</h1>
+      <board :score="score" :miss="miss"></board>
+      <goal></goal>
+      <ball :image="src" @score="score ++" @miss="miss ++"></ball>
+    </div>
+  </div>
+</template>
+
+<script>
+import Goal from './components/Goal.vue'
+import Ball from './components/Ball.vue'
+import Board from './components/Board.vue'
+export default {
+  components: {
+    Goal,
+    Ball,
+    Board
+  }, 
+
+  data () {
+    return {
+      score: 0,
+      miss: 0,
+      status: '',
+      title: "🐷 Let's play soccer 🐷",
+      goalClass: "something-great",
+      src: '/static/soccer-ball.png'
+    }
+  },
+
+  methods: {
+    // shoot () { 
+    //   const results = [
+    //     'miss',
+    //     'score'
+    //   ]
+
+    //   this.status = results[Math.floor(Math.random() * results.length)]
+
+    //   setTimeout(() => {
+    //     this.reset()
+    //   }, 800)
+    // },
+
+    // reset () {
+    //   this.status = 'reset'
+    //   setTimeout(() => {
+    //     this.status = ''
+    //   }, 500)
+    // },
+
+    // imageSrc(){
+    //   return '/static/soccer-ball.png'
+    // }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-workshop-master/src/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="board">
+        <div class="board-score">Your score is {{ score }}</div>
+        <div class="board-miss">You missed {{ miss }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'score',
+        'miss'
+    ]
+}
+</script>
+
+<style>
+
+</style>
+
